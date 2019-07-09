74b322f805ca39867ecbc1e7a4b0f9da9a5ee72c
Martin <martinfleig@hotmail.com>
2019-07-09 17:28:43 -0400

    Merge branch 'master' of https://github.com/mfleig/greengo

--- resources/views/admin/createProductForm.blade.php
+++ resources/views/admin/createProductForm.blade.php
@@ -23,34 +23,34 @@
         {{csrf_field()}}
 
         <div class="form-group">
-            <label for="name">Nombre</label>
-            <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre del producto" required>
+          <label for="name">Nombre</label>
+          <input type="text" class="form-control" name="nombre" id="nombre" placeholder="Nombre del producto" required>
         </div>
         <div class="form-group">
-            <label for="description">Descripcion</label>
-            <input type="text" class="form-control" name="descripcion" id="descripcion" placeholder="descripcion" required>
-        </div>
-
-        <div class="form-group">
-            <label for="image">Imagen</label>
-            <input type="file" class=""  name="imagen" id="imagen" required>
-        </div>
-        <div class="form-group">
-            <label for="type">Tipo</label>
-            <input type="text" class="form-control" name="tipo" id="tipo" placeholder="tipo" required>
+          <label for="description">Descripcion</label>
+          <input type="text" class="form-control" name="descripcion" id="descripcion" placeholder="descripcion" required>
         </div>
 
         <div class="form-group">
-            <label for="type">Precio</label>
-            <input type="text" class="form-control" name="precio" id="precio" placeholder="precio" required>
+          <label for="image">Imagen</label>
+          <input type="file" class=""  name="imagen" id="imagen" required>
         </div>
         <div class="form-group">
-            <label for="type">Stock</label>
-            <input type="text" class="form-control" name="stock" id="stock" placeholder="stock" required>
+          <label for="type">Tipo</label>
+          <input type="text" class="form-control" name="tipo" id="tipo" placeholder="tipo" required>
+        </div>
+
+        <div class="form-group">
+          <label for="type">Precio</label>
+          <input type="text" class="form-control" name="precio" id="precio" placeholder="precio" required>
         </div>
         <div class="form-group">
-            <label for="type">Categoria</label>
-        <select class="form-control" name="categoria_id" id="categoria_id">
+          <label for="type">Stock</label>
+          <input type="text" class="form-control" name="stock" id="stock" placeholder="stock" required>
+        </div>
+        <div class="form-group">
+          <label for="type">Categoria</label>
+          <select class="form-control" name="categoria_id" id="categoria_id">
 
             @foreach($categorias as $categoria)
             <option value="{{$categoria->id_categoria}}" >{{$categoria->nombre}}</option>
